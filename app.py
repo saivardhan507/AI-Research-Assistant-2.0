ef193a706a03de40a18ef9c191f9a37511a3c3d5
Sai Vardhan Reddy Eppa <119395872+saivardhan507@users.noreply.github.com>
2026-04-08 05:23:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_classic.chains.question_answering import load_qa_chain
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import speech_recognition as sr
 from gtts import gTTS
